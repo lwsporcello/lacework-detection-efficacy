82670fc8d286f34618344384e8a7678e315d97b3
sporcello7 <79470244+sporcello7@users.noreply.github.com>
2021-08-23 10:26:13 -0400

    Update lw-det-test.sh

--- lw-det-test.sh
+++ lw-det-test.sh
@@ -2,39 +2,52 @@
 # Script to simulate activity that will trigger Lacework events
 
 EXT_IP="54.184.116.123"
-FILENAME="lw-stage-1"
-BIN_URL="http://${EXT_IP}/bin/${FILENAME}"
+FILENAME="lw-stage-1-"
+BIN_URL="http://${EXT_IP}/bin/"
+
+get_os_type () {
+	case "$OSTYPE" in
+		darwin*)	OS_TYPE=darwin ;;
+		linux*)		OS_TYPE=linux ;;
+	esac
+}
 
 download_exec_binary() {
-        # Download the first stage binary
-        echo "Downloading ${FILENAME}..."
-        if [[ $(curl -s -w "%{http_code}\\n" -X GET ${BIN_URL} -o ${FILENAME}) -eq 200 ]]
+
+	# Download the first stage binary
+	echo "Downloading ${FILENAME}$1..."
+        if [[ $(curl -s -w "%{http_code}\\n" -X GET ${BIN_URL}${FILENAME}$1 -o ${FILENAME}$1) -eq 200 ]]
         then
-                echo " - Successfully downloaded ${FILENAME}!"
+                echo " - Successfully downloaded ${FILENAME}$1!"
         else
-                echo " - Failed to download ${FILENAME}!"
+                echo " - Failed to download ${FILENAME}$1!"
         fi
 
-        # Change permissions so it can be executed
-        echo "changing permissins on ${FILENAME} to allow execution..."
-        chmod u+x ${FILENAME}
-        if [[ $? -eq 0 ]]
-        then
-                echo " - Successfully changed permissions"
-        else
-                echo " - Failed to change permissions on ${FILENAME}!"
-        fi
+	# Change permissions so it can be executed
+	echo "Changing permissins on ${FILENAME}$1 to allow execution..."
+	chmod u+x ${FILENAME}$1
+	if [[ $? -eq 0 ]]
+	then
+		echo " - Successfully changed permissions"
+	else
+		echo " - Failed to change permissions on ${FILENAME}$1!"
+	fi
 
-        # Execute the binary
-        echo "Executing ${FILENAME}..."
-        ./${FILENAME} &
-        if [[ $? -eq 0 ]]
+	# Execute the binary
+	echo "Executing ${FILENAME}$1..."
+	./${FILENAME}$1 >/dev/null 2>&1 &
+	if [[ $? -eq 0 ]]
         then
-                echo " - Successfully executed ${FILENAME}!"
+                echo " - Successfully executed ${FILENAME}$1!"
         else
-                echo " - Failed to execute ${FILENAME}!"
+                echo " - Failed to execute ${FILENAME}$1!"
         fi
 }
 
-download_exec_binary
-echo "Script finished, terminating."
+# get os type
+get_os_type
+
+# download the right binary
+download_exec_binary $OS_TYPE
+
+echo "Script completed, terminating."
